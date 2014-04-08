2ddbb1c9e9e352e2ce4a3e7bfc59235e9ce66a06
Jaegermeister95 <nils.behrens@t-online.de>
2014-04-08 10:25:46 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,3 @@
- /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
      System.out.println("Hello World");
